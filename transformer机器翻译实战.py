334ecd4816774564839a1c2181596b15703360d4
ziyizhou <2059356794@qq.com>
2025-05-02 21:51:56 +0800

    111

--- transformer机器翻译实战.py
+++ transformer机器翻译实战.py
@@ -7,9 +7,6 @@
 
 
 
-
-
-
 # 构建代码关键的两点：
 # 1. 从整体到局部，先搭建大框架
 # 2. 搞清整个模型的流动中形状的改变
@@ -18,31 +15,40 @@
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
-        self.src_emb = nn.Embedding(src_vocab_size, d_model)
+        self.src_emb = nn.Embedding(src_vocab_size, d_model) # 生成词向量矩阵
         self.pos_emb = PositionalEncoding(d_model)
-        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
+        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])  #堆叠encoder
 
     def forward(self, enc_inputs):
-        
+        #  enc_inputs是一个batch的句子，形状为(batch_size,source_len)，source_len是编码器端输入的句子长度
+        enc_outputs = self.src_emb(enc_inputs)
+        enc_outputs = 
+
+class Decoder(nn.Module):
+
+    
 
 
-
-class DecoderLayer(nn.Module)  :
-
+# 在transformer中,分为三个部分，encoder编码层, decoder解码层, projection输出层
 class Transformer(nn.Model):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        # projection是一个线性层，将d_model维度映射到目标词汇表tgt_vocab_size的大小
+        self.projection = nn.Linear(d_model,tgt_vocab_size,bias = False)
 
-
-
-
-
-
-
+    def forward(self, enc_inputs, dec_inputs):
+        enc_outputs,enc_self_attns = self.encoder(enc_inputs)
+        # 解码器端输入和编码器端的输出送入解码器
+        dec_outputs,dec_self_attns,dec_enc_attns = self.decoder(dec_inputs,enc_inputs,enc_outputs)
+        dec_logits = self.projection(dec_outputs)
+        return dec_logits.view(-1,dec_logits.size(-1)),enc_self_attns,dec_self_attns,dec_enc_attns
 
 
 
 
 if __name__ == "__main__":
-    
 
     sentences = ['ich mochte ein bier P','S i want a beer','i want a beer E']
     src_vocab = {'P':0,'ich':1,'mochte':2,'ein':3,'bier':4}
